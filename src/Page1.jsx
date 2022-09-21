54351f73ce7ab5c2ba10685af4b5b381f2586003
koyamal <74910401+koyamal@users.noreply.github.com>
2022-09-22 08:25:22 +0900

    Change pages by js but link

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,14 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export const Page1 = () => {
   const arr = [...Array(100).keys()];
   console.log(arr);
+
+  const histroy = useHistory();
+
+  const onClickDetailA = () => {
+    return histroy.push("/page1/detailA");
+  };
   return (
     <div>
       <h1>Page 1!</h1>
       <Link to={{ pathname: "/page1/detailA", state: arr }}>DetailA</Link>
       <br />
       <Link to="/page1/detailB">DetailB</Link>
+      <br />
+      <button onClick={onClickDetailA}>DetailA</button>
     </div>
   );
 };
