d5aaaa4aa886c8854d310226dead53b9fa73633d
koyamal <74910401+koyamal@users.noreply.github.com>
2022-09-22 08:11:31 +0900

    Use Query parameters

--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,11 +1,16 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 
 export const UrlParameter = () => {
   const { id } = useParams();
+  const { search } = useLocation();
+  const query = new URLSearchParams(search);
+  console.log(query);
+
   return (
     <div>
       <h1>UrlParameter page!</h1>
       <p>ID is {id}</p>
+      <p>Query is {query.get("name")}</p>
     </div>
   );
 };
